2fd5d34c8da012252c46ef02261cc457f43c1f2b
Forster Asamany <asamanyforster@yahoo.com>
2023-03-23 04:23:59 +0000

    Setup MySQL server test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,6 @@
--- This script prepares a MySQL server for the project
--- Create project testing database with the name : hbnb_test_db
+-- creates database and user
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- Creating new user named : hbnb_test with all privileges on the db hbnb_test_db
--- with the password : hbnb_test_pwd if it dosen't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Grant the SELECT privilege for the user hbnb_test on the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
--- Grant all privileges to the new user on hbnb_test_db
+CREATE USER If NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
